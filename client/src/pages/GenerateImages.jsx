163182d7eaa8a0d674574eb6cb575c572fdf0ca9
priyanshubajpai1 <priyanshubajpai9920@gmail.com>
2025-08-11 21:46:29 +0530

    generateimages

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -109,7 +109,7 @@ const GenerateImages = () => {
                         <div className='text-sm flex flex-col items-center gap-5
                         text-gray-400'>
                             <Image className='w-9 h-9' />
-                            <p>Enter a prompt and click "Generate image " to get started</p>
+                            <p>Enter a topic and click "Generate image " to get started</p>
                         </div>
                     </div>
                 ) : (
