76a9c269173194c68c6a84d033633eeaef9c4111
priyanshubajpai1 <priyanshubajpai9920@gmail.com>
2025-08-11 22:37:17 +0530

    change

--- client/src/pages/GenerateImages.jsx
+++ client/src/pages/GenerateImages.jsx
@@ -24,7 +24,7 @@ const GenerateImages = () => {
         try {
             setLoading(true)
 
-            const prompt = `Generate am image of ${input} in the style ${selectedStyle}`
+            const prompt = `Generate an image of ${input} in the style ${selectedStyle}`
 
             const { data } = await axios.post('/api/ai/generate-image', {prompt, publish},
             {headers: {Authorization: `Bearer ${await getToken()}`}})
